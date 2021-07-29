019599bd1e2b07c54a4d77511d8d729682155b5a
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-07-29 11:55:58 -0400

    will publish
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- bin/pkg.js
+++ bin/pkg.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 exports.__esModule = true;
-var index_1 = require("../index");
+var index_1 = require('../index');
 function main() {
-    var result = index_1.pkg.get(process.argv[2], index_1.pkg.path(), false);
-    if (result !== 'undefined') {
-        console.log(result);
-        process.exit(0);
-    }
-    console.error((process.argv[2]) + " returned " + result + "}");
-    process.exit(1);
+  var result = index_1.pkg.get(process.argv[2], index_1.pkg.path(), false);
+  if (result !== 'undefined') {
+    console.log(result);
+    process.exit(0);
+  }
+  console.error(process.argv[2] + ' returned ' + result + '}');
+  process.exit(1);
 }
 main();
